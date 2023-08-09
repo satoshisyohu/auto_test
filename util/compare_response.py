3620307b231e1487f1cfb41a8cb2bc70594e167e
satoshisyohu <satoshisyohu@gmail.com>
2023-08-09 21:30:34 +0900

    response比較修正

--- util/compare_response.py
+++ util/compare_response.py
@@ -2,12 +2,25 @@ import jsondiff
 
 
 def main():
-    compareResponse()
+    resultLis = []
+    test1 = {"res": "account",
+             "res2": ["diec"],
+             "res1":
+                 {"res": "res",
+                  "test": "test"}}
+    test2 = {"res": "account",
+             "res2": ["diec"],
+             "res1":
+                 {"res": "res",
+                  "test": "tes1"}}
+
+    res = _compare(test1, test2, resultLis)
+    print(res)
 
 
 def compareResponse():
-    test1 = {"res": "account", "res2": ["diec"]}
-    test2 = {"res": "account", "res2": {"diec"}}
+    test1 = {"res": "account", "res2": ["diec"], "res1": {"res": "res", "test": "test"}}
+    test2 = {"res": "account", "res2": ["diec"], "res1": {"res": "res", "test": "test"}}
 
     if len(test1.keys()) == len(test2.keys()):
         for key in test1.keys():
@@ -15,6 +28,22 @@ def compareResponse():
             # print(test1.get(str(key)) == test2.get(str(key)))
     else:
         print("notSame")
+
+
+def _compare(test1, test2, resultList):
+    keys = test1.keys()
+    for key in keys:
+        if isinstance(test1.get(key), dict):
+            _compare(test1.get(key), test2.get(key), resultList)
+        else:
+            #ここで予約後の処理を入れていく
+            if test1.get(key) == test2.get(key):
+                resultList.append(True)
+                print("ok")
+            else:
+                print("ng")
+                resultList.append(False)
+    return resultList
 
 
 if __name__ == "__main__":
