b87d98a3befa7860d475bbb64f196f8dbfa05d90
satoshisyohu <satoshisyohu@gmail.com>
2023-08-16 20:50:31 +0900

    change deal timestamp

--- util/utils.py
+++ util/utils.py
@@ -1,3 +1,4 @@
+import math
 import re
 import uuid
 import datetime
@@ -9,11 +10,12 @@ UUID = '${UUID}'
 
 # timestamp
 CURRENT_TIMESTAMP = '${CURRENT_TIMESTAMP}'
-SPANNER_TIMESTAMP_PLUS_MINUS = '${CURRENT_TIMESTAMP'
+CURRENT_TIMESTAMP_PLUS_MINUS = '${CURRENT_TIMESTAMP'
 
 CURRENT_DATE = '${CURRENT_DATE}'
 CURRENT_DATE_PLUS_MINUS = '${CURRENT_DATE'
 
+JST = datetime.timezone(datetime.timedelta(hours=+9), 'JST')
 
 # 予め予約されている変数を置き換える処理。
 # 指定できる予約後はReadme を参照すること
@@ -24,7 +26,7 @@ def switch_reserve_words(target_word):
     res_target_word = ''
 
     # 正規表現で対象となる予約語を検索し、リストに格納する
-    change_words_list = re.findall(r"^\${[a-zA-Z0-9-+_]+}", target_word)
+    change_words_list = re.findall(r"^\${[a-zA-Z0-9-+_.]+}", target_word)
 
     # リストに格納された予約語を取り出し、対象に置き換えて行く。
     for change_word in change_words_list:
@@ -33,17 +35,17 @@ def switch_reserve_words(target_word):
         if UUID in change_word:
             res_target_word = target_word.replace(change_word, str(uuid.uuid4()))
         elif CURRENT_TIMESTAMP in change_word:
-            res_target_word = target_word.replace(change_word, datetime.datetime.now().isoformat())
+            res_target_word = target_word.replace(change_word, datetime.datetime.now(JST).isoformat())
         elif CURRENT_DATE in change_word:
             res_target_word = target_word.replace(change_word, datetime.date.today().isoformat())
 
-        elif SPANNER_TIMESTAMP_PLUS_MINUS in change_word:
-            editedTargetWord = str(change_word).replace("${CURRENT_TIMESTAMP", "").replace("}", "")
-            now = datetime.datetime.now()
+        elif CURRENT_TIMESTAMP_PLUS_MINUS in change_word:
+            editedTargetWord = str(change_word).replace(CURRENT_TIMESTAMP_PLUS_MINUS, "").replace("}", "")
+            now = datetime.datetime.now(JST)
             res_target_word = changeTime(editedTargetWord, now)
 
         elif CURRENT_DATE_PLUS_MINUS in change_word:
-            editedTargetWord = str(change_word).replace("${CURRENT_DATE", "").replace("}", "")
+            editedTargetWord = str(change_word).replace(CURRENT_DATE_PLUS_MINUS, "").replace("}", "")
             now = datetime.date.today()
             res_target_word = changeTime(editedTargetWord, now)
 
@@ -53,22 +55,33 @@ def switch_reserve_words(target_word):
 def changeTime(target, now):
     if "-" in target:
         days = target.replace("-", "")
-        targetTime = now - datetime.timedelta(days=int(days))
+        if '.' in days:
+            targetTime = dealFloat(days,now)
+        else:
+            targetTime = now - datetime.timedelta(days=int(days))
     else:
         days = target.replace("+", "")
-        targetTime = now + datetime.timedelta(days=int(days))
+        if '.' in days:
+            targetTime = dealFloat(days,now)
+        else:
+            targetTime = now + datetime.timedelta(days=int(days))
     return targetTime.isoformat()
+
+def dealFloat(target,now):
+    hour = math.floor(float(target) * 24)
+    return now + datetime.timedelta(hours=int(hour))
+
 
 
 def main():
     print("start")
 
-    target = '${UUID}'
+    target = '${CURRENT_TIMESTAMP+0.1}'
 
     # print(re.sub(r"\${\w+}", target,'test'))
     # print(re.search(r"/\${\w+}/", target)ß)
 
-    if re.search(r"^\${[a-zA-Z0-9-+_]+}", target) is not None:
+    if re.search(r"^\${[a-zA-Z0-9-+_.]+}", target) is not None:
         print(switch_reserve_words(target))
 
 
